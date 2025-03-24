9c71074184fa965ac80a249ad80957ac8b04faec
Samuel Razon <samuelrazon.g@gmail.com>
2025-03-24 15:06:52 -0600

    Inicialización del proyecto con React y Express

---
+++ backend/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+app.get('/', (req, res) => {
+  res.send('Backend funcionando!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
