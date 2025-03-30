50bbe57aa3bdc16e2ea6f6b995b5631624f75560
KillMidory <mariana.iniguez9142@alumnos.udg.mx>
2025-03-30 12:21:57 -0600

    [Front](fix) -> cambio para que funcione

--- frontend/src/Componentes/Inicioregistro/inicioregistro.jsx
+++ frontend/src/Componentes/Inicioregistro/inicioregistro.jsx
@@ -6,9 +6,9 @@ const inicioregistro = () => {
         <div className="container"> {/*Contenido de todo el inicio*/}
             <div className='header'> {/*Contenido principal*/}
                 <div className='article'>Camarones</div>{/*Apartado de la izquierda, el logo*/}
-                <div className='aside'>
+                <div className='aside'> {/*Contenido del formulario*/}
                     <div className='text'>Bienvenido</div> 
-                    <div className='inputs'> {/*Contenido del formulario*/}
+                    <div className='inputs'> 
                         <div className='input'>
                             <img src="" alt="" /> {/*Cambiar a la imagen que se utilizara*/}
                             <input type="text" placeholder='Nombre'/>
@@ -24,7 +24,7 @@ const inicioregistro = () => {
                     </div> {/*Fin del Div class inputs*/} 
                         <div className='forgot-password'> ¿Olvidaste la contraseña? <br/>
                             <span>Haz click aquí</span></div>
-                    <div className='submit-container'>
+                    <div className='submit-container'> {/*Botones*/}
                         <div className="submit">Inicio de sesión</div>
                         <div className="submit">Login</div>
                     </div>
