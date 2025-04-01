682db9eb6f64ebb65763827c358c3b84f5a2406f
StoicLogic <jose.lomeli5002@alumnos.udg.mx>
2025-04-01 14:37:44 -0600

    (feat) -> mejorando la vista principal de la pagina

--- frontend/src/pages/inicioregistro/inicioregistro.jsx
+++ frontend/src/pages/inicioregistro/inicioregistro.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 
 const Inicioregistro = () => {
     const [titulo, setTitulo] = useState(true); // Controla el texto (Bienvenido / Inicio de sesión)
-    const [mostrarNombre, setMostrarNombre] = useState(true); // Controla la visibilidad del campo Nombre
+    const [mostrarNombre, setMostrarNombre] = useState(false); // Controla la visibilidad del campo Nombre
     const [sesionregistro, setBoton] = useState(true); // Controla el testo del boton inicio de sesion
 
     const cambiarTexto = () => {
@@ -17,38 +17,39 @@ const Inicioregistro = () => {
 
     return (
         <div className="container"> {/* Contenido de todo el inicio */}
-            <div className='header'> {/* Contenido principal */}
-                <div className='article'>Camarones</div> {/* Apartado de la izquierda, el logo */}
-                <div className='aside'> {/* Contenido del formulario */}
-                    <div className='text'>{titulo ? "Bienvenido" : "Inicio de sesión"}</div> {/* Texto que cambiará */}
-                    <div className='inputs'>
-                        {mostrarNombre && (
-                            <div className='input'>
-                                <img src="" alt="" /> {/* Imagen pendiente */}
-                                <input type="text" placeholder='Nombre' />
-                            </div>
-                        )}
+            <div className='logo'></div> {/* Apartado de la izquierda, el logo */}
+            <div className='aside'> {/* Contenido del formulario */}
+                <div className='text'>{titulo ? "Inicio de sesión" : "Registrarse"}</div> {/* Texto que cambiará */}
+                <div className='inputs'>
+                    {mostrarNombre && (
                         <div className='input'>
                             <img src="" alt="" /> {/* Imagen pendiente */}
-                            <input type="email" placeholder='Correo electrónico' />
+                            <input type="text" placeholder='Nombre' />
                         </div>
-                        <div className='input'>
-                            <img src="" alt="" /> {/* Imagen pendiente */}
-                            <input type="password" placeholder='Contraseña' />
-                        </div>
-                    </div> 
-                    <div className='forgot-password'> 
-                        ¿Olvidaste la contraseña? <br/>
-                        <span>Haz click aquí</span>
+                    )}
+                    <div className='input'>
+                        <img src="" alt="" /> {/* Imagen pendiente */}
+                        <input type="email" placeholder='Correo electrónico' />
                     </div>
-                    <div className='submit-container'> {/* Botones */}
-                        <div className="submit" onClick={cambiarTexto}>{sesionregistro ? "Inicio de sesión" : "Registro"}</div>
-                        <Link to ={'/registro'}>
-                            <div className="submit">Login</div> {/*Como le muevo para cada uno?*/}
-                        </Link>
+                    <div className='input'>
+                        <img src="" alt="" /> {/* Imagen pendiente */}
+                        <input type="password" placeholder='Contraseña' />
                     </div>
+                </div> 
+                
+                <div className='submit-container'> {/* Botones */}
+                    <div className="submit" onClick={cambiarTexto}>{sesionregistro ? "Iniciar sesión" : "Registrarse"}</div>
+                    <Link to ={'/registro'}>
+                        <div className="submit">Login</div> {/*Como le muevo para cada uno?*/}
+                    </Link>
+                </div>
+
+                <div className='forgot-password'> 
+                    ¿Olvidaste la contraseña? <br/>
+                    <span>Haz click aquí</span>
                 </div>
             </div>
+    
         </div>
     );
 };
