679a82b5bc15695420d292414d16af20189c2a65
EmilianoTH <emiliano.delatorre9058@alumnos.udg.mx>
2025-04-03 00:42:24 -0600

    [Backend](fix) -> Arreglamos el esquema del usuario que se mandara a la base de datos de mongo.

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,9 +1,15 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
-const UserSchema = new mongoose.Schema({
-  username: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true }
-})
+const UsuarioSchema = new Schema({
+  correo: { type: String, required: true, unique: true }, // Correo obligatorio y único
+  contraseña: { type: String, required: true }, // Contraseña obligatoria
+  username: { type: String, required: true }, // URL o path de la foto
+  foto: { type: String}, // URL o path de la foto
 
-module.exports = mongoose.model('User', UserSchema)
+  // Referencias a rubrosDefault y rubrosPersonalizados
+  rubrosDefault: [{ type: Schema.Types.ObjectId, ref: 'RubroDefault' }], // Rubros default referenciados
+  rubrosPersonalizados: [{ type: Schema.Types.ObjectId, ref: 'RubroPersonalizado' }] // Rubros personalizados
+});
+
+module.exports = mongoose.model('Usuario', UsuarioSchema);
