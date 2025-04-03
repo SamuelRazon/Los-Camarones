2b40a3df1c00133e838e5f93c0d6134da8387baf
Samuel Razon <samuelrazon.g@gmail.com>
2025-04-03 16:14:55 -0600

    [Backend](fix) -> Correciones en las configuraciones de inicializacion

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,17 @@
-require('dotenv').config();
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const registroRouter = require('./routes/authRoutes');
+const express = require('express')
+const dotenv = require('dotenv')
+const cors = require('cors')
+const connectDB = require('./config/db')
+const authRoutes = require('./routes/authRoutes')
 
-const app = express();
-const PORT = process.env.PORT || 3000;
+dotenv.config()
+connectDB()
 
-mongoose.connect(process.env.MONGO_URI, { 
-  useNewUrlParser: true, 
-  useUnifiedTopology: true 
-})
-.then(() => console.log('Conectado a MongoDB Atlas'))
-.catch(err => console.error('Error al conectar a MongoDB Atlas', err));
+const app = express()
+app.use(express.json())
+app.use(cors())
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/api/auth', authRoutes)
 
-app.use('/api', registroRouter);
-
-app.listen(PORT, () => {
-  console.log(`Servidor corriendo en puerto ${PORT}`);
-});
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => console.log(`Server online in ${PORT}`))
