e449829cacc3aa36522c6f8000dc027f912fb332
Moy10000 <123517695+Moy10000@users.noreply.github.com>
2025-04-05 16:09:25 -0600

    [Backend](feat) ->Se crea Document.js

---
+++ backend/models/Document.js
@@ -0,0 +1,12 @@
+// De quien es la coso subida
+const mongoose = require('mongoose');
+
+const DocumentSchema = new mongoose.Schema({
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  rubro: { type: String, required: true },
+  s3Url: { type: String, required: true },
+  fileName: { type: String },
+  uploadedAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('Document', DocumentSchema);
