34956c7db83563220ccfd466f9786d2ff737ff9b
Moy10000 <123517695+Moy10000@users.noreply.github.com>
2025-04-05 15:14:32 -0600

    [Backend](feat) -> Configuracion de entorno S3

---
+++ backend/s3.js
@@ -0,0 +1,22 @@
+
+const AWS = require('aws-sdk');
+const { v4: uuidv4 } = require('uuid');
+require('dotenv').config();
+
+const s3 = new AWS.S3({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: process.env.AWS_REGION
+});
+
+const uploadFile = (file) => {
+  const params = {
+    Bucket: process.env.AWS_BUCKET_NAME,
+    Key: `${uuidv4()}-${file.originalname}`,
+    Body: file.buffer
+  };
+
+  return s3.upload(params).promise(); 
+};
+
+module.exports = { uploadFile };
