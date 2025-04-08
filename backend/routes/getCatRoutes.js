fc2c9a3bbdb5a1638585bc14ecdd0d541058e66f
EmilianoTH <emiliano.delatorre9058@alumnos.udg.mx>
2025-04-07 23:21:14 -0600

    [Backend](feat) -> Se crea la ruta que devolvera todos los rubros default y personalizados que corresponden a un usuario.

---
+++ backend/routes/getCatRoutes.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const Usuario = require('../models/User');
+const { authMiddleware } = require('../middleware/auth');
+
+const router = express.Router();
+
+router.get('/mycategories', authMiddleware, async (req, res) => {
+  try {
+    // Encontramos al usuario y poblamos los arrays de rubros
+    const usuario = await Usuario.findById(req.user.id)
+      .populate('RubroPersonalizado')  
+      .populate('RubroDefault');
+      
+      /* El metodo populate intercambia los object id que tenemos guardados dentro de los arrays 
+         rubrosdefault y personalizados por los documentos completos que corresponden a ese object id */
+
+    if (!usuario) {
+      return res.status(404).json({ error: 'Usuario no encontrado' });
+    }
+
+    /* Mapeo de todos los atributos de cada coleccion*/
+    const rubrosPersonalizados = usuario.rubrosPersonalizados.map(rubro => ({
+      id: rubro._id,
+      nombre: rubro.nombre,
+      propiedades: rubro.propiedades, // Array de propiedades
+      propiedadtipo: rubro.propiedadtipo,
+      propiedadesobligatorio: rubro.propiedadesobligatorio,
+    }));
+
+    const rubrosDefault = usuario.rubrosDefault.map(rubro => ({
+      id: rubro._id,
+      nombre: rubro.nombre,
+      propiedades: rubro.propiedades, // Array de propiedades
+      propiedadtipo: rubro.propiedadtipo,
+    }));
+
+    // Esto es para enviar los datos del mapeo al frontend
+    res.json({
+      rubrosPersonalizados,
+      rubrosDefault
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Error obteniendo los rubros del usuario' });
+  }
+});
+
+module.exports = router;
