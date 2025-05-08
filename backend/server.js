ec11ac3e45bf30a4acaa7108424001b4ac1b4312
EmilianoTH <emiliano.delatorre9058@alumnos.udg.mx>
2025-05-07 20:25:40 -0600

    [Backend](fix) -> Se arregla pequeño error.

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const dotenv = require('dotenv')
-//const cors = require('cors')
+const cors = require('cors')
 const connectDB = require('./config/db')
 const authRoutes = require('./routes/authRoutes')
 const perCatRoutes = require('./routes/perCatRoutes')
@@ -18,7 +18,7 @@ connectDB()
 
 const app = express()
 app.use(express.json())
-//app.use(cors())
+app.use(cors())
 
 app.use('/api/auth', authRoutes)
 app.use('/api/perCat', perCatRoutes)
